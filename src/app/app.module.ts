3edfa51b9f07a7e1cc2b873f97e51b6f9dd64a92
Victor <victor.ossa@hotmail.com>
2019-09-24 22:30:11 -0500

    Se despliega formuilario desde componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { OrderService } from './shared/order.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [OrderService],
   bootstrap: [AppComponent]
