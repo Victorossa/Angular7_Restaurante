36a5bf04ff8a2a414eb301834c2f17d6e7438d37
Victor <victor.ossa@hotmail.com>
2019-09-28 06:19:59 -0500

    Modif-OrderItems and Order service

--- src/app/shared/order.service.ts
+++ src/app/shared/order.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Order } from './order';
+import { OrderItem } from './order-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   formData:Order;
+  orderItem:OrderItem[];
 
   constructor() { }
 }
